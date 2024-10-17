8600f67fae084a1bc49dd295dfd7ca10611ae163
AnnaDays <analuizadias389@gmail.com>
2024-10-17 10:14:02 -0300

    Create ValoresMultiplos.java

---
+++ lista_de_exercicios/src/lista_de_exercicios/ValoresMultiplos.java
@@ -0,0 +1,25 @@
+package lista_de_exercicios;
+
+import java.util.Scanner;
+
+public class ValoresMultiplos {
+
+	public static void main(String[] args) {
+		
+		Scanner sc =  new Scanner(System.in);
+		int a, b;
+		
+		System.out.println("Digite o 1º número:");
+		a = sc.nextInt();
+		System.out.println("Digite o 2º número:");
+		b = sc.nextInt();
+		
+		if(a % b == 0 || b % a == 0) {
+			System.out.println("São Múltiplos!");
+		} else {
+			System.out.println("Não são Múltiplos!");
+		}
+
+	}
+
+}
